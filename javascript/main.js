c8abb408a316e1006afc90f5ee7a3c4d39682336
People Menschen <145572747+PeopleMenschen@users.noreply.github.com>
2023-12-01 15:06:25 +0100

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+// accordion button
+// const buttons = document.querySelectorAll(".accordion-button");
+// buttons.forEach((button) => {
+//     button.addEventListener("click", function () {
+//         const content = this.nextElementSibling;
+//         buttons.forEach((otherButton) => {
+//             const otherContent = otherButton.nextElementSibling;
+//             if (otherButton !== this) {
+//                 otherContent.style.display = "none";
+//             }
+//         });
+//         if (content.style.display === "block") {
+//             content.style.display = "none";
+//         } else {
+//             content.style.display = "block";
+//         }
+//     });
+// });
